796753c49fb0613bd42feca29bba0d4eb4a7efff
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-02-04 22:20:58 +0100

    installed dotenv

--- config/index.js
+++ config/index.js
@@ -2,7 +2,12 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 const connectDb = async () => {
-    mongoose.connect(`mongodb+srv://jae321:${process.env.DB_PASSWORD}@cluster0.gwvtatj.mongodb.net/test`).then(res => {
+    mongoose.connect(`mongodb+srv://jae321:${process.env.DB_PASSWORD}@cluster0.gwvtatj.mongodb.net/test`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    ).then(res => {
         return res
     }).catch(error => {
         return error
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const bodyParser = require("body-parser")
 
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
 	res.status(200).send({message: "Server is ok"})
